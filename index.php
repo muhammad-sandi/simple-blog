9b52a27c04901a698f9416013c1dc9c0648713e8
muhammad-sandi <muhammadsandi1911@gmail.com>
2024-04-30 19:20:35 +0700

    Publish

---
+++ index.php
@@ -0,0 +1,239 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Muhammad Sandi</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="img/logo.png">
+</head>
+
+<body>
+    <div id="wrap">
+        <div id="header">
+        </div>
+        <!--Header-->
+        <div id="menu">
+            <ul>
+                <li> <a href="index.php"><span>Beranda</span></a></li>
+                <li> <a href="#"><span>Tentang Kami</span></a></li>
+                <li> <a href="#"><span>Program Studi</span></a>
+                    <ul>
+                        <li> <a href="#"><span>Teknologi Informasi</span></a></li>
+                        <li> <a href="#"><span>Sistem Informasi</span></a></li>
+                        <li> <a href="#"><span>Teknik Penerbangan</span></a></li>
+                    </ul>
+                </li>
+                <li> <a href="#"><span>Jurnal Penelitian</span></a></li>
+                <li> <a href="#"><span>Gallery</span></a></li>
+                <li> <a href="#"><span>Kontak Kami</span></a></li>
+            </ul>
+        </div>
+        <!--Menu-->
+        <div id="left">
+            <div id="top-left">
+                <div id="title">
+                    <p>Berita Terbaru</p>
+                </div>
+                <!--title-->
+                <div id="content-tl">
+                    <div id="tl-wrap">
+                        <img src="img/Unsurya.jpg" width="100px" height="100px" alt="">
+                        <p><a href="#"></a><b>UNSURYA MENGUNJUNGI PERUSAHAAN DIJEPANG UNTUK KEPERLUAN MAGANG</b></p>
+                    </div>
+                    <div id="tl-wrap">
+                        <img src="img/Unsurya.jpg" width="100px" height="100px" alt="">
+                        <p><a href="#"></a><b>UNSURYA MENGUNJUNGI PERUSAHAAN DIJEPANG UNTUK KEPERLUAN MAGANG</b></p>
+                    </div>
+                </div>
+                <!--content-tl-->
+            </div>
+            <!--Top-left-->
+            <div id="top-midle">
+                <div id="title">
+                    <p>Kategori</p>
+                </div>
+                <div id="content-tm">
+                    <ul>
+                        <li><a href="#">Internal kampus</a></li>
+                        <li><a href="#">Kedirgantaraan</a></li>
+                        <li><a href="#">Tutorial</a></li>
+                        <li><a href="#">Ebook</a></li>
+                        <li><a href="#">Pembaca Menulis </a></li>
+                        <li><a href="#">Foto Kegiatan kampus</a></li>
+                        <li><a href="#">Serba Serbi Kampus</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div id="top-midle">
+                <div id="title">
+                    <p>Logo</p>
+                </div>
+                <div id="content-tm">
+                    <img src="img/logo.png" width="200px" height="200px" alt="">
+                </div>
+            </div>
+        </div>
+        <!--left-->
+
+        <div id="right">
+            <div id="top-right">
+                <div id="title">
+                    <p>Isi Konten</p>
+                </div>
+                <!--Title-->
+                <div id="content-tr">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo ratione modi quia atque labore
+                        officiis! Pariatur eligendi nesciunt, ad in, excepturi dolorum quo magnam voluptas quod
+                        quisquam, eum vitae omnis!
+                        Fugiat omnis velit totam optio consectetur sed provident enim voluptate esse nobis soluta
+                        laudantium excepturi cumque, ratione ipsum eius quidem non cupiditate tenetur, dolorum amet.
+                        Sequi explicabo quia tempora voluptates!
+                        Doloribus, quam maxime autem omnis illo reprehenderit voluptatem tenetur minus et ratione amet
+                        sequi, tempora sint magni fugit. Incidunt ea modi doloribus quos placeat temporibus cupiditate,
+                        at atque sequi unde!
+                        Consectetur, atque modi. Obcaecati dicta eos vel odit illum voluptate? Aspernatur earum quaerat
+                        itaque nihil ad architecto recusandae odit similique porro rem adipisci, ducimus qui harum iste
+                        inventore, error dolorem.
+                    </p>
+                </div>
+                <!--content-tr-->
+            </div>
+            <!--top right-->
+            <div id="banner">
+                <img src="img/html.png" alt="Gambar" width="100px">
+                <img src="img/css.png" alt="Gambar" width="100px">
+                <img src="img/js.png" alt="Gambar" width="100px">
+            </div>
+            <div id="middle-right">
+                <div id="box-mr">
+                    <div id="title">
+                        <p>Artikel Populer</p>
+                    </div>
+                    <div id="page-boxmr">
+                        <ul>
+                            <li>
+                                <p><img src="img/himi.jpg" width="34" height="37" align="left" hspace="10" /> <a
+                                        href="#">Lorem ipsum dolor sit amet consectetur adipisicing elit.</a></p>
+                            </li>
+                            <li>
+                                <p><img src="img/himi.jpg" width="34" height="37" align="left" hspace="10" /> <a
+                                        href="#">Lorem ipsum dolor sit amet consectetur adipisicing elit.</a></p>
+                            </li>
+                            <li>
+                                <p><img src="img/himi.jpg" width="34" height="37" align="left" hspace="10" /> <a
+                                        href="#">Lorem ipsum dolor sit amet consectetur adipisicing elit.</a></p>
+                            </li>
+                            <li>
+                                <p><img src="img/himi.jpg" width="34" height="37" align="left" hspace="10" /> <a
+                                        href="#">Lorem ipsum dolor sit amet consectetur adipisicing elit.</a></p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div id="box-middle">
+                    <div id="title">
+                        <p>Pengumuman</p>
+                    </div>
+                    <div id="page-boxmr">
+                        <li>
+                            <img src="img/himi.jpg" width="34" height="37" align="left" hspace="10" />
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                        </li>
+                        <li>
+                            <img src="img/himi.jpg" width="34" height="37" align="left" hspace="10" />
+                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
+                        </li>
+                        <li>
+                            <img src="img/himi.jpg" width="34" height="37" align="left" hspace="10" />
+                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
+                        </li>
+                        <li>
+                            <img src="img/himi.jpg" width="34" height="37" align="left" hspace="10" />
+                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
+                        </li>
+                    </div>
+                </div>
+                <div id="box-mr1">
+                    <div id="title">
+                        <p>Badan Eksekutif Mahasiswa</p>
+                    </div>
+                    <div id="page-boxmr">
+                        <center>
+                            <a href="profile.php" target="_blank"><img src="img/bem.jpg" height="215px"></a>
+                        </center>
+                    </div>
+                </div>
+            </div>
+            <!--Midle Right-->
+            <div id="bottom-right">
+                <div id="box-br">
+                    <div id="title">
+                        <p>Artikel Populer</p>
+                    </div>
+                    <div id="page-boxbr">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia nobis ratione amet optio
+                            minima qui, hic deserunt inventore similique, asperiores iste quo exercitationem nesciunt a
+                            perferendis. Molestiae enim adipisci esse eaque voluptatibus dolorum praesentium facilis ut
+                            earum aut sed magni aliquid impedit, nam qui accusantium reprehenderit totam quam
+                            laudantium.</p>
+                    </div>
+                </div>
+                <div id="box-br2">
+                    <div id="title">
+                        <p>Sponsor</p>
+                    </div>
+                    <div id="page-boxbr">
+                        <img src="img/boeing.png" alt="" width="200px" height="200px">
+                    </div>
+                </div>
+
+            </div>
+            <!--Bottom Right-->
+        </div>
+        <!--right-->
+
+        <div id="bottom">
+            <div id="top-bottom1">
+                <div id="title">
+                    <p>Logo</p>
+                </div>
+                <div id="content-tb">
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius quis magnam ipsa, libero
+                        numquam velit nesciunt harum explicabo dolorum rerum quos sunt ad porro tenetur consequatur
+                        labore officiis error alias?</p>
+                </div>
+            </div>
+            <div id="top-bottom2">
+                <div id="title">
+                    <p>Logo</p>
+                </div>
+                <div id="content-tb">
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius quis magnam ipsa, libero
+                        numquam velit nesciunt harum explicabo dolorum rerum quos sunt ad porro tenetur consequatur
+                        labore officiis error alias?</p>
+                </div>
+            </div>
+            <div id="top-bottom3">
+                <div id="title">
+                    <p>Logo</p>
+                </div>
+                <div id="content-tb">
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius quis magnam ipsa, libero
+                        numquam velit nesciunt harum explicabo dolorum rerum quos sunt ad porro tenetur consequatur
+                        labore officiis error alias?</p>
+                </div>
+            </div>
+        </div>
+
+        <div id="footer">
+            <p>Copyright &copy; Muhammad Sandi | 2024 | All Rights Reserved</p>
+        </div>
+        <!--footer-->
+    </div>
+    <!---wrap-->
+
+</body>
+
+</html>
